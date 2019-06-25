1564d581b2248318ce358bbee20eecf1808c89b9
Alexandre Lotte <alexlotte16@gmail.com>
2019-06-25 19:38:18 -0400

    cleanup

--- src/app/view.jsx
+++ src/app/view.jsx
@@ -1,31 +1,21 @@
 
 import { Router, Link } from 'hyperapp-site-generator'
 
-const container = {
-  maxWidth: '640px',
-  margin: '3rem auto',
-  padding: '1rem'
-}
-
 // Root application view
 export default (state) => (
-  <div style={container}>
+  <div class="container">
     <header>
-      <nav>
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/counter">Counter</Link>
-        </ul>
-      </nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/counter">Counter</Link>
     </header>
     <main>
       {Router(state)}
     </main>
     <footer>
-      <a href="https://github.com/loteoo/hyperapp-site-generator-starter" target="_blank">Boilerplate</a>
-      <a href="https://github.com/loteoo/hyperapp-site-generator" target="_blank">Generator</a>
-      <a href="https://hyperapp-site-generator-demo.netlify.com/" target="_blank">Demo site</a>
+      <a href="https://github.com/loteoo/hyperapp-site-generator-starter" target="_blank" rel="noopener noreferrer">Boilerplate repository</a>
+      <a href="https://github.com/loteoo/hyperapp-site-generator" target="_blank" rel="noopener noreferrer">Generator repository</a>
+      <a href="https://hyperapp-site-generator-demo.netlify.com/" target="_blank" rel="noopener noreferrer">Generator demo site</a>
     </footer>
   </div>
 )
