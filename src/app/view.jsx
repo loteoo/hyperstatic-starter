fb229cba014dc8aeafb8fae07c7144fb67ccc1c3
Alexandre Lotte <alexlotte16@gmail.com>
2019-06-25 12:23:07 -0400

    Add router and links

--- src/app/view.jsx
+++ src/app/view.jsx
@@ -1,19 +1,14 @@
-import { SetA, SetB } from './actions'
 
-const decodeInput = event => parseInt(event.target.value || 0)
-
-const container = {
-  maxWidth: '768px',
-  margin: '3rem auto'
-}
+import { Router, Link } from 'hyperapp-site-generator'
 
 // Root application view
-export default state => (
-  <main style={container}>
-    <h1>Do more with less</h1>
-    <input type="number" value={state.a} oninput={[SetA, decodeInput]} />
-    <input type="number" value={state.b} oninput={[SetB, decodeInput]} />
-    <h2>{state.a} + {state.b} = {state.a + state.b}</h2>
-    <pre><code>state: {JSON.stringify(state, null, 2)}</code></pre>
-  </main>
+export default (state) => (
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/counter">Counter</Link>
+    </nav>
+    {Router(state)}
+  </div>
 )
